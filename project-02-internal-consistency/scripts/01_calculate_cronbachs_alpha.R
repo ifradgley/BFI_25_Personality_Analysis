366e8df6ce2701448c3aece2cd8dc8ad26b6f3bc
ifradgley <india.fradgley@gmail.com>
2026-02-16 03:33:20 +0000

    Formatted code

--- project-02-internal-consistency/scripts/01_calculate_cronbachs_alpha.R
+++ project-02-internal-consistency/scripts/01_calculate_cronbachs_alpha.R
@@ -1,5 +1,5 @@
 # ================================
-# 01.calculate_cronbachs_alpha.R - 
+# 01.calculate_cronbachs_alpha.R -
 # ================================
 
 # Empty list that will hold the five Cronbachs alpha values
@@ -7,7 +7,7 @@ alpha_list <- list(
   agreeableness_items = c(),
   conscientiousness_items = c(),
   extraversion_items = c(),
-  neuroticism_items= c(),
+  neuroticism_items = c(),
   openness_items = c()
 )
 
@@ -16,14 +16,16 @@ subscales_alpha <- list(
   agreeableness_items = c("A1", "A2", "A3", "A4", "A5"),
   conscientiousness_items = c("C1", "C2", "C3", "C4", "C5"),
   extraversion_items = c("E1", "E2", "E3", "E4", "E5"),
-  neuroticism_items= c("N1", "N2", "N3", "N4", "N5"),
+  neuroticism_items = c("N1", "N2", "N3", "N4", "N5"),
   openness_items = c("O1", "O2", "O3", "O4", "O5")
 )
 
 # Loop through each subscale item
 for (subscale in names(subscales_alpha)) {
- # Calculate the Cronbachs alpha of each subscale
- alpha_list[[subscale]] <- psych::alpha(bfi_clean[, subscales_alpha[[subscale]]]) 
+  # Calculate the Cronbachs alpha of each subscale
+  alpha_list[[subscale]] <- psych::alpha(bfi_clean[, subscales_alpha[[
+    subscale
+  ]]])
 }
 
 # Extract key statistics from each subscale's alpha object
@@ -33,7 +35,7 @@ all_alphas <- data.frame(
   raw_alphas = sapply(alpha_list, function(item) item$total$raw_alpha),
   std_alphas = sapply(alpha_list, function(item) item$total$std.alpha),
   avg_r = sapply(alpha_list, function(item) item$total$average_r)
-  )
+)
 
 # Remove "_items" suffix from subscale names for cleaner display
 
@@ -41,12 +43,15 @@ all_alphas$subscale <- gsub("_items", "", all_alphas$subscale)
 
 # Format results into table
 
-knitr::kable(all_alphas,
-             digits = 2,
-             row.names = FALSE,
-             col.names = c("Subscale", "Raw α", "Std α", "Avg r"),
-             caption = "<span style='font-size: 16px; font-weight: bold;'>Cronbach's Alpha by Subscale</span>"
-             ) %>%
-  kable_styling(bootstrap_options = c("striped", "hover"),
-                full_width = FALSE) %>%
-                column_spec(1, bold = TRUE)
+knitr::kable(
+  all_alphas,
+  digits = 2,
+  row.names = FALSE,
+  col.names = c("Subscale", "Raw α", "Std α", "Avg r"),
+  caption = "<span style='font-size: 16px; font-weight: bold;'>Cronbach's Alpha by Subscale</span>"
+) %>%
+  kable_styling(
+    bootstrap_options = c("striped", "hover"),
+    full_width = FALSE
+  ) %>%
+  column_spec(1, bold = TRUE)
